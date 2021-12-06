adc5006bf695398d43a3d3b1f03f0befbec962bc
Peter Harrison <pmch2@cam.ac.uk>
2021-12-06 12:03:49 +0000

    Implement add_none

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 def clean(x: list):
     x = remove_excluded(x)
+    x = add_none(x)
     return x
 
 def remove_excluded(x):
     excluded = ["banana", "orange"]
     return [obj for obj in x if obj not in excluded]
+
+def add_none(x):
+    if None not in x:
+        x = x + [None]
+    return x
